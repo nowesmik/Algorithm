a2f73d96dc28cf779d9531fcdf9bbc168e2bf971
nowesmik <76905272+nowesmik@users.noreply.github.com>
2023-03-01 00:27:21 +0900

    add files

---
+++ BOJ/GREEDY/2217_로프.py
@@ -0,0 +1,13 @@
+n = int(input())
+lope = []
+result = []
+
+for _ in range(n):
+    lope.append(int(input()))
+
+lope.sort(reverse=True)
+
+for i in range(n):
+    result.append((i + 1) * lope[i])
+
+print(max(result))
