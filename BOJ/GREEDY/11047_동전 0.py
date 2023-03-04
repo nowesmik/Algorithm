8581ca3cf62d12340434e852109a5029b77d3779
nowesmik <76905272+nowesmik@users.noreply.github.com>
2023-03-05 01:35:45 +0900

    add files

---
+++ BOJ/GREEDY/11047_동전 0.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+coin = []
+cnt = 0
+
+for _ in range(n):
+    coin.append(int(input()))
+
+coin.sort(reverse=True)
+
+for i in coin:
+    if k // i > 0:
+        cnt += (k // i)
+        k = k % i
+
+print(cnt)
